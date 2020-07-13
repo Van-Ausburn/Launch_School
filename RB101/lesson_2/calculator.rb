8572cfc4dd1950dc8d5b7ff412903dc15dbbd751
Van-Ausburn <hausburn@gmail.com>
2020-07-13 11:15:19 -0400

    added calculator.rb

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -11,4 +11,23 @@ Kernel.puts("What is the first number?: ")
 
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("The number is : #{number1}! ")
+Kernel.puts("What is the second number?: ")
+
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? \
+\
+1) add 2) subtract 3) multiply 4) divide : ")
+
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else
+  result = number1.to_f() / number2.to_f()
+end
+Kernel.puts("The result is #{result.round(2)}")
