1628b516aa8fd4ee60f60adf49ee7b0ac9b3b66e
Van-Ausburn <hausburn@gmail.com>
2020-07-13 08:34:06 -0400

    repaired file system

---
+++ RB101/lesson_2/calculator.rb
@@ -0,0 +1,14 @@
+# calculator.rb
+
+# ask the user for 2 numbers
+# ask user for operation to perform
+# perform the operation on the numbers
+# output the result
+
+Kernel.puts("Welcome to the calculator!")
+
+Kernel.puts("What is the first number?: ")
+
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("The number is : #{number1}! ")
