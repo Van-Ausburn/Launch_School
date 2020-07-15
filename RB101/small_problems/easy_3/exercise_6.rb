2e3503ca3289b147cb8dca64ba797e68128190de
Van-Ausburn <hausburn@gmail.com>
2020-07-15 17:19:20 -0400

    added exercise_6.rb

--- RB101/small_problems/easy_3/exercise_6.rb
+++ RB101/small_problems/easy_3/exercise_6.rb
@@ -54,7 +54,12 @@ Algorithm:
 */ 
 
 //Code:
+def xor?(one, two)
+  return true if one && !two
+  return true if two && !one
 
+  false
+end
 
 //Test Cases: 
 xor?(5.even?, 4.even?) == true
@@ -76,3 +81,8 @@ p xor?(5.even?, 4.even?) #== true
 p xor?(5.odd?, 4.odd?) #== true
 p xor?(5.odd?, 4.even?) #== false
 p xor?(5.even?, 4.odd?) #== false
+
+# Further Exploration
+# This couldn't be a short circuit, it has to read both to make a decision
+# If value 1 is true value 2 will have to be checked and be false for an overall truth
+# both sides have to be checked for a determination to be made.
