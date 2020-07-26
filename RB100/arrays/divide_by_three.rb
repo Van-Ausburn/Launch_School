7f4be66af8cae8dbe3423c97b1fc9411b11cf090
Van-Ausburn <hausburn@gmail.com>
2020-07-26 09:27:38 -0400

    updated divide_by_three.rb

--- RB100/arrays/divide_by_three.rb
+++ RB100/arrays/divide_by_three.rb
@@ -3,6 +3,6 @@
 numbers = [5, 9, 21, 26, 39]
 
 divisible_by_three = numbers.select do |number|
-                       number % 3 == 0
-                     end
+  (number % 3).zero?
+end
 p divisible_by_three
