c9de9afac3bc38ea8e61c9f538d971d4260fe003
Van-Ausburn <hausburn@gmail.com>
2020-07-26 09:29:54 -0400

    udated doubled.rb

--- RB100/arrays/doubled.rb
+++ RB100/arrays/doubled.rb
@@ -3,6 +3,6 @@
 numbers = [1, 2, 3, 4, 5, 6]
 
 doubled_numbers = numbers.map do |number|
-                    number * 2
-                  end
-p doubled_numbers
+  number * 2
+end
+p doubled_numbers == [2, 4, 6, 8, 10, 12]
