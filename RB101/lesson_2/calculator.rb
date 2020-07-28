3b467dcbd30d9dfad59c0b1ae8c3fdb1fa21c5c8
Van-Ausburn <hausburn@gmail.com>
2020-07-28 10:01:32 -0400

    removed text

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -1,10 +1,5 @@
 # calculator.rb
 
-# ask the user for 2 numbers
-# ask user for operation to perform
-# perform the operation on the numbers
-# output the result
-
 Kernel.puts("Welcome to the calculator!")
 
 Kernel.puts("What is the first number?: ")
