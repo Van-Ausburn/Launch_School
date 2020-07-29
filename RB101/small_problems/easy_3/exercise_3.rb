0a02fc13186fc315a75dd1bf03f016d30956e951
Van-Ausburn <hausburn@gmail.com>
2020-07-29 14:05:21 -0400

    cleaned file

--- RB101/small_problems/easy_3/exercise_3.rb
+++ RB101/small_problems/easy_3/exercise_3.rb
@@ -1,51 +1,10 @@
 # exercise_3.rb
 
 # Counting the Number of Characters
-# Write a program that will ask a user for an 
-# input of a word or multiple words and give 
-# back the number of characters. Spaces should 
+# Write a program that will ask a user for an
+# input of a word or multiple words and give
+# back the number of characters. Spaces should
 # not be counted as a character.
-
-# input:
-
-# Please write word or multiple words: walk
-# output:
-
-# There are 4 characters in "walk".
-# input:
-
-# Please write word or multiple words: walk, don't run
-# output:
-
-# There are 13 characters in "walk, don't run".
-
-=begin
-Input: 
-Output: 
-
-Problem: 
-  -  Ask user for string input
-     Given a string or series of strings
-     Iterate through the string or strings
-     Count the number of characters in the string or strings
-     Spaces can not be counted
-
-Clarifying Questions: 
-  - 
-
-Data Structure: 
-  -  Array of strings
-
-Algorithm: 
-  -  Get input from user, ask for a string or strings
-*/ 
-
-//Code:
-
-
-//Test Cases: 
-
-=end
 
 def prompt(message)
   puts "==> #{message}"
