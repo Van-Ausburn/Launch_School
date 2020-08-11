c09ec33ae21202dd891e9c7e232ade6b5d13c6b1
Van-Ausburn <hausburn@gmail.com>
2020-08-11 12:56:20 -0400

    update

--- RB100/conditionals/sleep_alert.rb
+++ RB100/conditionals/sleep_alert.rb
@@ -1,10 +1,10 @@
 # sleep_alert.rb
 
-status = [ 'awake', 'tired' ].sample
+status = ['awake', 'tired'].sample
 
 alert = if status == 'awake'
-         'Be productive!'
-        else status
+          'Be productive!'
+        else
           'Go to sleep!'
         end
 
