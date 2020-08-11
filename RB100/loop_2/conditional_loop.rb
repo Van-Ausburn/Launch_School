7bf16724d4424b519a415746f02e16e8822c0130
Van-Ausburn <hausburn@gmail.com>
2020-08-11 13:41:54 -0400

    update

--- RB100/loop_2/conditional_loop.rb
+++ RB100/loop_2/conditional_loop.rb
@@ -3,20 +3,10 @@
 process_the_loop = [true, false].sample
 
 loop do
-  if process_the_loop == true
+  if process_the_loop
     puts "The loop was processed!"
   else
     puts "The loop wasn't processed!"
   end
   break
 end
-
-
-# if process_the_loop
-#   loop do
-#     puts "The loop was processed!"
-#     break
-#   end
-# else
-#   puts "The loop wasn't processed!"
-# end
