5bc23e70087d975c62fa23934f61518a02813904
Van-Ausburn <hausburn@gmail.com>
2020-08-12 11:54:52 -0400

    complete

--- RB101/small_problems/easy_4/exercise_1.rb
+++ RB101/small_problems/easy_4/exercise_1.rb
@@ -16,6 +16,6 @@ def short_long_short(string1, string2)
   end
 end
 
-p short_long_short('abc', 'defgh') == "abcdefghabc"
-p short_long_short('abcde', 'fgh') == "fghabcdefgh"
-p short_long_short('', 'xyz') == "xyz"
+p short_long_short('abc', 'defgh') #== "abcdefghabc"
+p short_long_short('abcde', 'fgh') #== "fghabcdefgh"
+p short_long_short('', 'xyz') #== "xyz"
