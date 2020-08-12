64fac140ef38c678200eb477f9a35d134b550e85
Van-Ausburn <hausburn@gmail.com>
2020-08-12 11:53:27 -0400

    complete

--- RB101/small_problems/easy_3/exercise_7.rb
+++ RB101/small_problems/easy_3/exercise_7.rb
@@ -1,7 +1,6 @@
 # exercise_7.rb
 
 =begin
-
 Odd Lists
 Write a method that returns an Array that
 contains every other element of an Array
@@ -9,7 +8,6 @@ that is passed in as an argument. The
 values in the returned list should be
 those values that are in the 1st, 3rd, 5th,
 and so on elements of the argument Array.
-
 Problem:
   - given an array of integers
   - iterate integers and return 1st, 3rd, 5th ... elements of the array
@@ -38,16 +36,13 @@ Pseudocode:
 Algorithm:
   -
 */
-
 //Test Cases:
 oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
 oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
 oddities(['abc', 'def']) == ['abc']
 oddities([123]) == [123]
 oddities([]) == []
-
 //Code:
-
 =end
 
 def oddities(int_array)
